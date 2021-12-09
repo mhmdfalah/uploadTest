1c8f938a868773dfb02e1a6403c63afab6031628
mhmdfalah <42360170+mhmdfalah@users.noreply.github.com>
2021-12-09 11:48:14 +0300

    starter project

---
+++ config.js
@@ -0,0 +1,13 @@
+const config = {
+    db: { 
+      host:  '151.80.165.232',
+      port:'3306',
+      user: 'alifaaza_homes',
+      password:  'PSZu}QL.C8t-',
+      database:  'alifaaza_imageUpload',
+    },
+    root:__dirname
+  };
+  
+  
+  module.exports = config;
